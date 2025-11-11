d9d71f98e03e65e0aee3f04725c691b7cefec384
Arely Rosendes <141471552+arelyyrosendes@users.noreply.github.com>
2025-11-10 21:00:47 -0800

    Set base path for React portfolio project

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/React-Personal-Portfolio/',
   plugins: [react()],
   server: {
     port: 3000,
